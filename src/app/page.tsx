8a95a35d0c913db723ada1ee55a300454e3d166a
Yan Moe Naing <yan.webdev.1234@gmail.com>
2024-01-17 07:19:40 +0630

    ch7 vocabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,162 +6,162 @@ import { useState } from "react";
 import _, { flatMap, words } from "lodash";
 
 const list: Vocabs = [
-  // {
-  //   word: "きります",
-  //   meaning: "ဖြတ်သည်။ ညှပ်သည်။ လှီးသည်။",
-  //   hideWord: false,
-  //   hideMeaning: false,
-  // },
-  // {
-  //   word: "おくります",
-  //   meaning: "ပို့သည်။",
-  //   hideWord: false,
-  //   hideMeaning: false,
-  // },
-  // {
-  //   word: "あげます",
-  //   meaning: "ပေးသည်",
-  //   hideWord: false,
-  //   hideMeaning: false,
-  // },
-  // {
-  //   word: "もらいます",
-  //   meaning: "လက်ခံရယူသည်",
-  //   hideWord: false,
-  //   hideMeaning: false,
-  // },
-  // {
-  //   word: "かします",
-  //   meaning: "ချေးငှားသည် (ချေးပေးသည်၊ ငှားပေးသည်)",
-  //   hideWord: false,
-  //   hideMeaning: false,
-  // },
-  // {
-  //   word: "かります",
-  //   meaning: "ချေးငှားသည် (ချေးယူသည်၊ ငှားယူသည်)",
-  //   hideWord: false,
-  //   hideMeaning: false,
-  // },
-  // {
-  //   word: "おしえます",
-  //   meaning: "သင်ပေးသည်",
-  // },
-  // {
-  //   word: "ならいます",
-  //   meaning: "သင်ယူသည်",
-  // },
-  // {
-  //   word: "かけます[でんわをー]",
-  //   meaning: "ဆက်သည် (တယ်လီဖုန်းကို)",
-  // },
-  // {
-  //   word: "て",
-  //   meaning: "လက်",
-  // },
-  // {
-  //   word: "はし",
-  //   meaning: "တူ",
-  // },
-  // {
-  //   word: "スプーン",
-  //   meaning: "‌ဇွန်း",
-  // },
-  // {
-  //   word: "ナイフ",
-  //   meaning: "ဓား",
-  // },
-  // {
-  //   word: "フｵーク",
-  //   meaning: "‌‌ခက်ရင်း",
-  // },
-  // {
-  //   word: "はさみ",
-  //   meaning: "ကတ်ကြေး",
-  // },
-  // {
-  //   word: "パソコン",
-  //   meaning: "ကွန်ပြူတာ",
-  // },
-  // {
-  //   word: "ケータイ",
-  //   meaning: "မိုဘိုင်းဖုန်း",
-  // },
-  // {
-  //   word: "メール",
-  //   meaning: "‌အိးမေးလ်",
-  // },
-  // {
-  //   word: "ねんがじょう",
-  //   meaning: "နယူးရီးယားပို့စကတ်",
-  // },
-  // {
-  //   word: "パンチ",
-  //   meaning: "‌‌စက္ကူအပေါက်ဖောက်စက်",
-  // },
-  // {
-  //   word: "ホッチキス",
-  //   meaning: "စတာပလာ",
-  // },
-  // {
-  //   word: "セロテープ",
-  //   meaning: "ဆိုလိုတိပ်, ကပ်တိပ်",
-  // },
-  // {
-  //   word: "けしゴム",
-  //   meaning: "ခဲဖျတ်",
-  // },
-  // {
-  //   word: "かみ",
-  //   meaning: "‌စက္ကူ",
-  // },
-  // {
-  //   word: "はな",
-  //   meaning: "ပန်း",
-  // },
-  // {
-  //   word: "シャツ",
-  //   meaning: "‌‌ရှပ်အင်ကျီ",
-  // },
-  // {
-  //   word: "プレゼン",
-  //   meaning: "လက်ဆောင်",
-  // },
-  // {
-  //   word: "にもつ",
-  //   meaning: "အထုပ်အပိုး",
-  // },
-  // {
-  //   word: "おかわ",
-  //   meaning: "ပိုက်ဆံ",
-  // },
-  // {
-  //   word: "きっぷ",
-  //   meaning: "လက်မှတ်",
-  // },
-  // {
-  //   word: "クリスマス",
-  //   meaning: "‌ခရစ္စမတ်",
-  // },
-  // {
-  //   word: "ちち",
-  //   meaning: "‌‌အဖေ",
-  // },
-  // {
-  //   word: "はは",
-  //   meaning: "အမေ",
-  // },
-  // {
-  //   word: "おとうさん",
-  //   meaning: "အဖေ",
-  // },
-  // {
-  //   word: "おかうさん",
-  //   meaning: "အမေ",
-  // },
-  // {
-  //   word: "もう",
-  //   meaning: "‌နှင့်ပြီးပြီ (ပြီးနှင့်ပြီးပြီဟူသောအဓိပ္ပါယ်ကိုဆိုလိုသည်)",
-  // },
+  {
+    word: "きります",
+    meaning: "ဖြတ်သည်။ ညှပ်သည်။ လှီးသည်။",
+    hideWord: false,
+    hideMeaning: false,
+  },
+  {
+    word: "おくります",
+    meaning: "ပို့သည်။",
+    hideWord: false,
+    hideMeaning: false,
+  },
+  {
+    word: "あげます",
+    meaning: "ပေးသည်",
+    hideWord: false,
+    hideMeaning: false,
+  },
+  {
+    word: "もらいます",
+    meaning: "လက်ခံရယူသည်",
+    hideWord: false,
+    hideMeaning: false,
+  },
+  {
+    word: "かします",
+    meaning: "ချေးငှားသည် (ချေးပေးသည်၊ ငှားပေးသည်)",
+    hideWord: false,
+    hideMeaning: false,
+  },
+  {
+    word: "かります",
+    meaning: "ချေးငှားသည် (ချေးယူသည်၊ ငှားယူသည်)",
+    hideWord: false,
+    hideMeaning: false,
+  },
+  {
+    word: "おしえます",
+    meaning: "သင်ပေးသည်",
+  },
+  {
+    word: "ならいます",
+    meaning: "သင်ယူသည်",
+  },
+  {
+    word: "かけます[でんわをー]",
+    meaning: "ဆက်သည် (တယ်လီဖုန်းကို)",
+  },
+  {
+    word: "て",
+    meaning: "လက်",
+  },
+  {
+    word: "はし",
+    meaning: "တူ",
+  },
+  {
+    word: "スプーン",
+    meaning: "‌ဇွန်း",
+  },
+  {
+    word: "ナイフ",
+    meaning: "ဓား",
+  },
+  {
+    word: "フｵーク",
+    meaning: "‌‌ခက်ရင်း",
+  },
+  {
+    word: "はさみ",
+    meaning: "ကတ်ကြေး",
+  },
+  {
+    word: "パソコン",
+    meaning: "ကွန်ပြူတာ",
+  },
+  {
+    word: "ケータイ",
+    meaning: "မိုဘိုင်းဖုန်း",
+  },
+  {
+    word: "メール",
+    meaning: "‌အိးမေးလ်",
+  },
+  {
+    word: "ねんがじょう",
+    meaning: "နယူးရီးယားပို့စကတ်",
+  },
+  {
+    word: "パンチ",
+    meaning: "‌‌စက္ကူအပေါက်ဖောက်စက်",
+  },
+  {
+    word: "ホッチキス",
+    meaning: "စတာပလာ",
+  },
+  {
+    word: "セロテープ",
+    meaning: "ဆိုလိုတိပ်, ကပ်တိပ်",
+  },
+  {
+    word: "けしゴム",
+    meaning: "ခဲဖျတ်",
+  },
+  {
+    word: "かみ",
+    meaning: "‌စက္ကူ",
+  },
+  {
+    word: "はな",
+    meaning: "ပန်း",
+  },
+  {
+    word: "シャツ",
+    meaning: "‌‌ရှပ်အင်ကျီ",
+  },
+  {
+    word: "プレゼン",
+    meaning: "လက်ဆောင်",
+  },
+  {
+    word: "にもつ",
+    meaning: "အထုပ်အပိုး",
+  },
+  {
+    word: "おかわ",
+    meaning: "ပိုက်ဆံ",
+  },
+  {
+    word: "きっぷ",
+    meaning: "လက်မှတ်",
+  },
+  {
+    word: "クリスマス",
+    meaning: "‌ခရစ္စမတ်",
+  },
+  {
+    word: "ちち",
+    meaning: "‌‌အဖေ",
+  },
+  {
+    word: "はは",
+    meaning: "အမေ",
+  },
+  {
+    word: "おとうさん",
+    meaning: "အဖေ",
+  },
+  {
+    word: "おかうさん",
+    meaning: "အမေ",
+  },
+  {
+    word: "もう",
+    meaning: "‌နှင့်ပြီးပြီ (ပြီးနှင့်ပြီးပြီဟူသောအဓိပ္ပါယ်ကိုဆိုလိုသည်)",
+  },
   {
     word: "すてきですね",
     meaning: "‌လှတယ်နော်/လှလိုက်တာနော်",
@@ -186,6 +186,22 @@ const list: Vocabs = [
     word: "しつれいします",
     meaning:
       "‌ခွင့်ပြုပါ။(တစ်ဉီးတစ်ယောက်ထံမှ တစ်စုံတစ်ခုကိုတောင်းဆိုရာတွင်အသုံးပြုသည်)",
+  },
+  {
+    word: "[~は] いかがですか",
+    meaning: "‌(ဆို) ဘယ်လို(သဘောရ)လဲ။ /သုံးဆောင်မလား။",
+  },
+  {
+    word: "いただきます",
+    meaning: "‌စားပါတော့မယ်",
+  },
+  {
+    word: "ごちそうさま",
+    meaning: "ကျွေးမွေးပြုစုမှုအတွက် ကျေးဇူးတင်ပါတယ်။ (စားပီးခါစပြောရန်)",
+  },
+  {
+    word: "スペイン",
+    meaning: "စပိန်",
   },
 ];
 
