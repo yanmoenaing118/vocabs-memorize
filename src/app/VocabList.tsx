1f72fb0dba1855775d40da55eeb6477dcba4b548
Yan Moe Naing <yan.webdev.1234@gmail.com>
2024-01-20 23:06:53 +0630

    add data

--- src/app/VocabList.tsx
+++ src/app/VocabList.tsx
@@ -4,10 +4,10 @@ import Container from "./Container";
 type Vocab = {
   word: string;
   meaning: string;
-  hideWord?: boolean,
-  hideMeaning?: boolean,
-  conversation?: boolean,
-  ch: number
+  hideWord?: boolean;
+  hideMeaning?: boolean;
+  conversation?: boolean;
+  ch: number;
 };
 
 export type Vocabs = Vocab[];
@@ -21,7 +21,10 @@ export default function VocabList({ list }: VocabListProps) {
     <>
       <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {list.map((item) => (
-          <div key={item.word} className="glass-card bg-white bg-opacity-20 p-4 md:p-8 rounded-lg shadow-lg">
+          <div
+            key={item.word + item.ch}
+            className="glass-card bg-white bg-opacity-20 p-4 md:p-8 rounded-lg shadow-lg"
+          >
             <h1 className="text-sm sm:text-sm md:text-base lg:text-2xl font-light mb-4 text-black">
               {!item.hideWord && item.word}
             </h1>
